a067034ef6851e45035ee9aa8e19afa7d9fbc85d
Tim <tim.spiele1@freenet.de>
2023-07-19 03:32:33 +0200

    update
    new pictures
    deleted the standard site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'timmellentin.de';
+export class AppComponent implements OnInit {
+  images = ['black-agend-cat.jpg', 'cat-astronaut.jpg', 'cat-bussines.jpg', 'garage-cat.jpg', 'glass-cat.jpg', 'villa-cat.jpg']
+  showImage = true;
+  currentImage = 0
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      if (this.images.length - 1 == this.currentImage) {
+        this.currentImage = 0;
+        this.showImage = false
+        setTimeout(() => {
+          this.showImage = true;
+        }, 10)
+      }
+      else if (this.currentImage < this.images.length) {
+        this.currentImage++
+        this.showImage = false
+        setTimeout(() => {
+          this.showImage = true;
+        }, 10)
+      }
+    }, 5000)
+  }
 }
