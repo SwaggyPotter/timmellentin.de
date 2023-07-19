65569c6999cc0dc99ef26d304f1b3847b9b531dd
Tim <tim.spiele1@freenet.de>
2023-07-19 04:16:53 +0200

    add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SliderShowComponent } from './slider-show/slider-show.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SliderShowComponent
+    SliderShowComponent,
+    HeaderMenuComponent
   ],
   imports: [
     BrowserModule,
